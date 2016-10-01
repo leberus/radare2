leberus/radare2

commit 7fe4084b328ae4a25931d865da360b39a149af2c

update linux_coredump.c check nullptr for  elf_proc_note->n_threads
.

libr/debug/p/native/linux/linux_coredump.c
@@ -1470,6 +1470,7 @@ bool linux_generate_corefile (RDebug *dbg, RBuffer *dest) {
 	bool error = false;
 	size_t note_section_size, maps_size = 0;
 	int n_segments;
+	int n_threads;
 	ut32 hdr_size;
 	elf_offset_t offset = 0;
 
@@ -1485,6 +1486,10 @@ bool linux_generate_corefile (RDebug *dbg, RBuffer *dest) {
 	proc_data->per_process = get_proc_process_content (dbg);
 	if (!proc_data) {
 		free (elf_proc_note);
+		return false;
+	}
+	if (!elf_proc_note->n_threads || elf_proc_note->n_threads < 1 ) {
+		eprintf ("problem in elf_proc_note\n");
 		return false;
 	}
 	elf_proc_note->n_threads = proc_data->per_process->num_threads;

commit 78d9b6dd8610790cdc8095ebb518a50dcbfd205a

Fix: fix regression introduced by commit 7fe4084b328ae4a25931d865da360b39a149af2c

libr/debug/p/native/linux/linux_coredump.c
@@ -844,6 +844,7 @@ static proc_per_process_t *get_proc_process_content (RDebug *dbg) {
 
 	buff = r_file_slurp (file, &size);
 	if (!buff) {
+		eprintf ("error: /proc/%d/start does not exist\n", dbg->pid);
 		return NULL;
 	}
 
@@ -871,6 +872,7 @@ static proc_per_process_t *get_proc_process_content (RDebug *dbg) {
 	file = sdb_fmt (0, "/proc/%d/status", dbg->pid);
 	buff = r_file_slurp (file, &size);
 	if (!buff) {
+		eprintf ("error: /proc/%d/status does not exist\n", dbg->pid);
 		free (p);
 		return NULL;
 	}
@@ -1519,13 +1521,7 @@ bool linux_generate_corefile (RDebug *dbg, RBuffer *dest) {
 		free (proc_data);
 		return false;
 	}
-	if (!elf_proc_note->n_threads || elf_proc_note->n_threads < 1 ) {
-		eprintf ("problem in elf_proc_note\n");
-		free (elf_proc_note);
-		free (proc_data->per_process);
-		free (proc_data);
-		return false;
-	}
+	
 	elf_proc_note->n_threads = proc_data->per_process->num_threads;
 
 	/* Get NT_ process_wide: AUXV, MAPS, PRPSINFO */